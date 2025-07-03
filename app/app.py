d0c4ea2235c8ee7fb9482d4c5a97fa6c1c9a298a
risyadaddiva <95023054+risyadaddiva@users.noreply.github.com>
2025-07-03 19:20:32 +0700

    Update app.py

--- app/app.py
+++ app/app.py
@@ -127,8 +127,5 @@ def download_stego():
 
 if __name__ == '__main__':
     import os
-    port = int(os.environ.get('PORT', 5000))
-    app.run(debug=False, host='0.0.0.0', port=port)
-
-def get_app():
-    return app  # Flask instance
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=False, host="0.0.0.0", port=port)
